87db6222f3c2fddad899409c8f076277a8b1aaf1
beane <bdeanemachine@gmail.com>
2020-12-04 23:53:08 -0800

    cleaner but sadder

--- day05/main.rb
+++ day05/main.rb
@@ -1,23 +1,15 @@
 class BoardingPass
-  attr_reader :row_code, :column_code
+  attr_reader :seat_id
   def initialize(ticket_code)
-    @row_code = ticket_code[0..6].split('')
-    @column_code = ticket_code[7..-1].split('')
-  end
-
-  def row
-    row_code.map { |code| code == 'F' ? 0 : 1 }.join.to_i(2)
-  end
-
-  def column
-    column_code.map { |code| code == 'L' ? 0 : 1 }.join.to_i(2)
-  end
-
-  def seat_id
-    row * 8 + column
+    @seat_id = ticket_code
+      .gsub('F', '0')
+      .gsub('B', '1')
+      .gsub('L', '0')
+      .gsub('R', '1')
+      .to_i(2)
   end
 end
- 
+
 # part 1
 puts File.readlines('input')
   .map(&:strip)
