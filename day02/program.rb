a5ec4121254426560af18d4e18a00140e4138f5c
beane <bdeanemachine@gmail.com>
2020-12-04 23:54:20 -0800

    tiny day 2 improvement

--- day02/program.rb
+++ day02/program.rb
@@ -1,10 +1,8 @@
 count = File.readlines('input').count do |line|
   rule, letter, password = line.split(' ', 3)
-  i, j = rule.split('-')
+  i, j = rule.split('-').map(&:to_i)
   letter = letter[0...-1]
-  i = i.to_i - 1
-  j = j.to_i - 1
-  (password[i] == letter) ^ (password[j] == letter) # bitwise xor. sorry...
+  (password[i-1] == letter) ^ (password[j-1] == letter) # bitwise xor. sorry...
   # password.count(letter).between?(min,max)
 end
 puts count
